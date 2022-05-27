3201d10c0d1b53763d333a364eebed254504fea2
thewoe <80894954+thewoe@users.noreply.github.com>
2022-05-27 17:00:49 +0100

    Integrated Display
    Displays total to console

--- TugDisplay.js
+++ TugDisplay.js
@@ -1,7 +1,10 @@
-class TugDisplay {
+export default class Display {
+    total = 0;
     // alert function -> Given an integer number
-    
+    alert = (number) => this.total += number;
     // class must sum the numbers
 
     // display function -> adds numbers up and then returns the total
+    display = () => console.log(this.total);
+    
 }
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Imports
 import NumberPublisher from './NumberPublisher.js';
-//import GraemeDisplay from './DisplayGraeme.js';
+import TugDisplay from './TugDisplay.js';
 
 // Instantiate publisher
 const numberPublisher = new NumberPublisher("RandomIntegerRandomInterval");
 
 // Instantiate observers and subscribe
-//const graeme = new GraemeDisplay('Graeme');
-//numberPublisher.registerSubscriber(graeme);
+const tug = new TugDisplay('Tug');
+numberPublisher.registerSubscriber(tug);
 
 // Run publisher
 numberPublisher.run();
@@ -15,5 +15,5 @@ numberPublisher.run();
 // Terminate publisher and display results
 const interval = 10000; // Ten seconds
 setTimeout(() => numberPublisher.stop(), interval);
-//setTimeout(() => graeme.display(), interval + 100);
+setTimeout(() => tug.display(), interval + 100);
 console.log("app.js completed");
