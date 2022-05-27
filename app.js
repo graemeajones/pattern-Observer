203964e493a79376fa61e6520794572daff77339
k1915361 <67165821+k1915361@users.noreply.github.com>
2022-05-27 17:17:31 +0100

    Added eugene display (#3)

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Imports
 import NumberPublisher from './NumberPublisher.js';
 import TugDisplay from './TugDisplay.js';
+import EugeneDisplay from './EugeneDisplay.js';
 
 // Instantiate publisher
 const numberPublisher = new NumberPublisher("RandomIntegerRandomInterval");
 
 // Instantiate observers and subscribe
 const tug = new TugDisplay('Tug');
+const eugene = new EugeneDisplay('Eugene');
 numberPublisher.registerSubscriber(tug);
+numberPublisher.registerSubscriber(eugene);
 
 // Run publisher
 numberPublisher.run();
@@ -16,4 +19,5 @@ numberPublisher.run();
 const interval = 10000; // Ten seconds
 setTimeout(() => numberPublisher.stop(), interval);
 setTimeout(() => tug.display(), interval + 100);
+setTimeout(() => eugene.display(), interval + 100);
 console.log("app.js completed");
