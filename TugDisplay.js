0b28b97566bfbe84a216f0b2d4c4f740b1eb657f
thewoe <80894954+thewoe@users.noreply.github.com>
2022-05-27 16:47:01 +0100

    Initialised package and class

---
+++ TugDisplay.js
@@ -0,0 +1,7 @@
+class TugDisplay {
+    // alert function -> Given an integer number
+    
+    // class must sum the numbers
+
+    // display function -> adds numbers up and then returns the total
+}
