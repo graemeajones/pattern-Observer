7b7da6fbdc325721768d65077307cd25ed63f4d6
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-05-28 01:41:51 +0100

    Evolve Pattern
    Added getNumber method to NumberPublisher class to support a pull-approach in addition to push approach. Added a GraemeDisplay class with subscribeTo method which stores reference to publisher to facilitate using the publishers getNumber method.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import NumberPublisher from './NumberPublisher.js';
 import TugDisplay from './TugDisplay.js';
 import EugeneDisplay from './EugeneDisplay.js';
+import GraemeDisplay from './GraemeDisplay.js';
 
 // Instantiate publisher
 const numberPublisher = new NumberPublisher("RandomIntegerRandomInterval");
@@ -9,8 +10,10 @@ const numberPublisher = new NumberPublisher("RandomIntegerRandomInterval");
 // Instantiate observers and subscribe
 const tug = new TugDisplay('Tug');
 const eugene = new EugeneDisplay('Eugene');
+const graeme = new GraemeDisplay('Graeme');
 numberPublisher.registerSubscriber(tug);
 numberPublisher.registerSubscriber(eugene);
+graeme.subscribeTo(numberPublisher);
 
 // Run publisher
 numberPublisher.run();
@@ -20,4 +23,5 @@ const interval = 10000; // Ten seconds
 setTimeout(() => numberPublisher.stop(), interval);
 setTimeout(() => tug.display(), interval + 100);
 setTimeout(() => eugene.display(), interval + 100);
+setTimeout(() => graeme.display(), interval + 100);
 console.log("app.js completed");
